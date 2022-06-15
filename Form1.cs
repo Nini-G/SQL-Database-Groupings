3b9b1adcd634ddeb8f287d87a5c83b730182755b
Nini-G <josephinegamit13@gmail.com>
2022-06-15 12:52:16 +0800

    Add the add button code

--- Form1.cs
+++ Form1.cs
@@ -39,5 +39,23 @@ namespace SQLdatabase
             dataGridView1.DataSource = dt;
             con.Close();
         }
+
+        //Add Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=NBPC1958\\SQLEXPRESS;Initial Catalog=employeeSalary;Integrated Security=True");
+            con.Open();
+            int a = Convert.ToInt32(textBox1.Text);
+            string b = textBox2.Text;
+            int c = Convert.ToInt32(textBox3.Text);
+            cmd = new SqlCommand("Insert Into Employee_Salary_Table values ('" + a + "','" + b + "', '" + c + "')", con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("One Record Added");
+            Display();
+            con.Close();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
     }
 }
