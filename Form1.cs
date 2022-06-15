15c251f77186c253d276cf373b7cb44054abdf69
Nini-G <josephinegamit13@gmail.com>
2022-06-15 12:50:44 +0800

    Add Form Load and Display class and using DataTable

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Data.SqlClient;
 
 namespace SQLdatabase
@@ -18,6 +19,25 @@ namespace SQLdatabase
             Application.Run(new Form1());
         }
 
+        //Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+            Display();
+        }
 
+        //Create Display class
+        private void Display()
+        {
+            con = new SqlConnection("Data Source=NBPC1958\\SQLEXPRESS;Initial Catalog=employeeSalary;Integrated Security=True");
+            con.Open();
+            cmd = new SqlCommand("Select * from Employee_Salary_Table", con);
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
     }
 }
